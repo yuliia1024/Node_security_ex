92d6ce133d2780d526c1a6bde83c9f09486392e4
yuliia1024 <yuliia.kvlnk@gmail.com>
2020-08-07 20:01:13 +0300

    set proper http headers with helmet

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import helmet from "helmet";
 import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 3000;
 
+//helmet setup
+app.use(helmet());
 // mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/CRMdb', {
-    useMongoClient: true
-});
+mongoose.connect('mongodb://localhost/CRMdb',);
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -6,6 +6,10 @@ const Contact = mongoose.model('Contact', ContactSchema);
 export const addNewContact = (req, res) => {
     let newContact = new Contact(req.body);
 
+    //validator for email
+
+
+
     newContact.save((err, contact) => {
         if (err) {
             res.send(err);
