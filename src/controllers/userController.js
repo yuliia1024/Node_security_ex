1b9f4c4419f760b381e830fd52a5287825115676
yuliia1024 <yuliia.kvlnk@gmail.com>
2020-08-09 19:55:26 +0300

    Add the handlers for login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,7 @@ import { UserSchema } from '../models/crmUsers';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
+
 const User = mongoose.model("User", UserSchema);
 
 export const loginRequired=(req,res,next)=>{
@@ -13,7 +14,7 @@ export const loginRequired=(req,res,next)=>{
 
 }
 
-export const register = (req,res,next)=>{
+export const register = (req,res, next)=>{
     const newUser = new User(req.body);
     newUser.hashPassword = bcrypt.hashSync(req.body.password, 10);
     newUser.save((err, user)=>{
@@ -23,7 +24,24 @@ export const register = (req,res,next)=>{
         } else{
             user.hashPassword = undefined;
             return res.json(user);
-
+            next();
         }
     })
+}
+export const login = (req,res) => {
+    User.findOne({
+        email: req.body.email
+    }, (err, user) => {
+        if (err) throw err;
+        if (!user) {
+            res.status(401).json({ message: 'Authentication failed. No user found'});
+        } else if (user) {
+            if (!user.comparePassword(req.body.password, user.hashPassword)) {
+                res.status(401).json({ message: 'Authentication failed. Wrong password'});
+            } else {
+                return res.json({token: jwt.sign({ email: user.email, username: user.username, _id: user.id},
+                        `MySuperSecret`)});
+            }
+        }
+    });
 }
